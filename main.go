c67f6ff00b0c2c90c0550e4171956ba307a9660f
Diego Carretero-Frades <carter@diektronics.com>
2014-10-23 00:29:38 -0700

    first try at server

--- main.go
+++ main.go
@@ -5,10 +5,11 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"time"
 
 	"diektronics.com/carter/dl/cfg"
 	"diektronics.com/carter/dl/dl"
-	"diektronics.com/carter/dl/types"
+	"diektronics.com/carter/dl/server"
 )
 
 var cfgFile = flag.String(
@@ -24,26 +25,30 @@ func main() {
 		log.Fatal(err)
 	}
 
-	d := dl.New(c, 5)
-	d.Download(&types.Download{Name: "The Hobbit The Desolation of Smaug (2013) EXTENDED",
-		Links: []*types.Link{
-			&types.Link{URL: "http://www.uploadable.ch/file/HFvfndwqKKdj/thtdos13.ex.1080p-gec.part01.rar"},
-			&types.Link{URL: "http://www.uploadable.ch/file/RshFX8MnFcYB/thtdos13.ex.1080p-gec.part02.rar"},
-			&types.Link{URL: "http://www.uploadable.ch/file/v2xUG3bV3dR4/thtdos13.ex.1080p-gec.part03.rar"},
-			&types.Link{URL: "http://www.uploadable.ch/file/8phcAxphQEJR/thtdos13.ex.1080p-gec.part04.rar"},
-			&types.Link{URL: "http://www.uploadable.ch/file/cUvU9z8jdmJ5/thtdos13.ex.1080p-gec.part05.rar"},
-			&types.Link{URL: "http://www.uploadable.ch/file/PfV3QpwNuCJV/thtdos13.ex.1080p-gec.part06.rar"},
-			&types.Link{URL: "http://www.uploadable.ch/file/yRkh2NkUhxDP/thtdos13.ex.1080p-gec.part07.rar"},
-			&types.Link{URL: "http://www.uploadable.ch/file/U7VtJBwK4r4x/thtdos13.ex.1080p-gec.part08.rar"},
-			&types.Link{URL: "http://www.uploadable.ch/file/PB3rG9R5N7tr/thtdos13.ex.1080p-gec.part09.rar"},
-			&types.Link{URL: "http://www.uploadable.ch/file/NrcrC9Ps9YKB/thtdos13.ex.1080p-gec.part10.rar"},
-			&types.Link{URL: "http://www.uploadable.ch/file/CVFrewUMWQ2B/thtdos13.ex.1080p-gec.part11.rar"},
-			&types.Link{URL: "http://www.uploadable.ch/file/t8pVzjrKvMcp/thtdos13.ex.1080p-gec.part12.rar"},
-			&types.Link{URL: "http://www.uploadable.ch/file/hX2cHG5c5NNu/thtdos13.ex.1080p-gec.part13.rar"},
-			&types.Link{URL: "http://www.uploadable.ch/file/fGzxbgEPspBD/thtdos13.ex.1080p-gec.part14.rar"},
-		}})
+	// TODO(diek): do recovery, all that is RUNNING becomes QUEUED, then all that is QUEUED goes to the queue.
+	server.New(dl.New(c, 5)).Run()
+
+	// d.Download(&types.Download{Name: "The Hobbit The Desolation of Smaug (2013) EXTENDED",
+	// 	Links: []*types.Link{
+	// 		&types.Link{URL: "http://www.uploadable.ch/file/HFvfndwqKKdj/thtdos13.ex.1080p-gec.part01.rar"},
+	// 		&types.Link{URL: "http://www.uploadable.ch/file/RshFX8MnFcYB/thtdos13.ex.1080p-gec.part02.rar"},
+	// 		&types.Link{URL: "http://www.uploadable.ch/file/v2xUG3bV3dR4/thtdos13.ex.1080p-gec.part03.rar"},
+	// 		&types.Link{URL: "http://www.uploadable.ch/file/8phcAxphQEJR/thtdos13.ex.1080p-gec.part04.rar"},
+	// 		&types.Link{URL: "http://www.uploadable.ch/file/cUvU9z8jdmJ5/thtdos13.ex.1080p-gec.part05.rar"},
+	// 		&types.Link{URL: "http://www.uploadable.ch/file/PfV3QpwNuCJV/thtdos13.ex.1080p-gec.part06.rar"},
+	// 		&types.Link{URL: "http://www.uploadable.ch/file/yRkh2NkUhxDP/thtdos13.ex.1080p-gec.part07.rar"},
+	// 		&types.Link{URL: "http://www.uploadable.ch/file/U7VtJBwK4r4x/thtdos13.ex.1080p-gec.part08.rar"},
+	// 		&types.Link{URL: "http://www.uploadable.ch/file/PB3rG9R5N7tr/thtdos13.ex.1080p-gec.part09.rar"},
+	// 		&types.Link{URL: "http://www.uploadable.ch/file/NrcrC9Ps9YKB/thtdos13.ex.1080p-gec.part10.rar"},
+	// 		&types.Link{URL: "http://www.uploadable.ch/file/CVFrewUMWQ2B/thtdos13.ex.1080p-gec.part11.rar"},
+	// 		&types.Link{URL: "http://www.uploadable.ch/file/t8pVzjrKvMcp/thtdos13.ex.1080p-gec.part12.rar"},
+	// 		&types.Link{URL: "http://www.uploadable.ch/file/hX2cHG5c5NNu/thtdos13.ex.1080p-gec.part13.rar"},
+	// 		&types.Link{URL: "http://www.uploadable.ch/file/fGzxbgEPspBD/thtdos13.ex.1080p-gec.part14.rar"},
+	// 	}})
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
 	<-signalChan
+	log.Println("entering lame duck mode for 2 seconds")
+	time.Sleep(2 * time.Second)
 }
---
+++ server/server.go
@@ -0,0 +1,94 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"diektronics.com/carter/dl/dl"
+	"diektronics.com/carter/dl/types"
+	"github.com/gorilla/mux"
+)
+
+const PathPrefix = "/down/"
+
+type Server struct {
+	d *dl.Downloader
+}
+
+func New(d *dl.Downloader) *Server {
+	return &Server{d: d}
+}
+
+func (s *Server) Run() {
+	// 1. Register paths
+	r := mux.NewRouter()
+	r.HandleFunc(PathPrefix, errorHandler(s.listDowns)).Methods("GET")
+	r.HandleFunc(PathPrefix, errorHandler(s.newDown)).Methods("POST")
+	r.HandleFunc(PathPrefix+"{id}", errorHandler(s.getDown)).Methods("GET")
+
+	http.Handle(PathPrefix, r)
+	http.Handle("/", http.FileServer(http.Dir("src/diektronics.com/carter/dl/server/static")))
+	// 2. Run server
+	go http.ListenAndServe(":4444", nil)
+}
+
+// badRequest is handled by setting the status code in the reply to StatusBadRequest.
+type badRequest struct{ error }
+
+// notFound is handled by setting the status code in the reply to StatusNotFound.
+type notFound struct{ error }
+
+// errorHandler wraps a function returning an error by handling the error and returning a http.Handler.
+// If the error is of the one of the types defined above, it is handled as described for every type.
+// If the error is of another type, it is considered as an internal error and its message is logged.
+func errorHandler(f func(w http.ResponseWriter, r *http.Request) error) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := f(w, r)
+		if err == nil {
+			return
+		}
+		switch err.(type) {
+		case badRequest:
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		case notFound:
+			http.Error(w, "task not found", http.StatusNotFound)
+		default:
+			log.Println(err)
+			http.Error(w, "oops", http.StatusInternalServerError)
+		}
+	}
+}
+
+func (s *Server) listDowns(w http.ResponseWriter, r *http.Request) error {
+	downs, err := s.d.Db().GetAll()
+	if err != nil {
+		return err
+	}
+	res := struct{ Downs []*types.Download }{downs}
+	return json.NewEncoder(w).Encode(res)
+}
+
+func (s *Server) newDown(w http.ResponseWriter, r *http.Request) error { return nil }
+func (s *Server) getDown(w http.ResponseWriter, r *http.Request) error {
+	id, err := parseID(r)
+	log.Println("Down is ", id)
+	if err != nil {
+		return badRequest{err}
+	}
+	down, err := s.d.Db().Get(id)
+	if err != nil {
+		return notFound{}
+	}
+	return json.NewEncoder(w).Encode(down)
+}
+
+func parseID(r *http.Request) (int64, error) {
+	txt, ok := mux.Vars(r)["id"]
+	if !ok {
+		return 0, fmt.Errorf("down id not found")
+	}
+	return strconv.ParseInt(txt, 10, 0)
+}
