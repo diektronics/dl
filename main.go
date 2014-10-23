a36ad6711768aa887b3a4b324076b155374a4044
Diego Carretero-Frades <carter@diektronics.com>
2014-10-22 22:24:25 -0700

    test with links

--- main.go
+++ main.go
@@ -24,8 +24,21 @@ func main() {
 	}
 
 	d := dl.New(c, 5)
-	d.Download(&types.Download{Name: "test1", Links: []*types.Link{
-		&types.Link{URL: "http://example.com/1.rar"},
-		&types.Link{URL: "http://example.com/2.rar"},
-	}})
+	d.Download(&types.Download{Name: "The Hobbit The Desolation of Smaug (2013) EXTENDED",
+		Links: []*types.Link{
+			&types.Link{URL: "http://www.uploadable.ch/file/HFvfndwqKKdj/thtdos13.ex.1080p-gec.part01.rar"},
+			&types.Link{URL: "http://www.uploadable.ch/file/RshFX8MnFcYB/thtdos13.ex.1080p-gec.part02.rar"},
+			&types.Link{URL: "http://www.uploadable.ch/file/v2xUG3bV3dR4/thtdos13.ex.1080p-gec.part03.rar"},
+			&types.Link{URL: "http://www.uploadable.ch/file/8phcAxphQEJR/thtdos13.ex.1080p-gec.part04.rar"},
+			&types.Link{URL: "http://www.uploadable.ch/file/cUvU9z8jdmJ5/thtdos13.ex.1080p-gec.part05.rar"},
+			&types.Link{URL: "http://www.uploadable.ch/file/PfV3QpwNuCJV/thtdos13.ex.1080p-gec.part06.rar"},
+			&types.Link{URL: "http://www.uploadable.ch/file/yRkh2NkUhxDP/thtdos13.ex.1080p-gec.part07.rar"},
+			&types.Link{URL: "http://www.uploadable.ch/file/U7VtJBwK4r4x/thtdos13.ex.1080p-gec.part08.rar"},
+			&types.Link{URL: "http://www.uploadable.ch/file/PB3rG9R5N7tr/thtdos13.ex.1080p-gec.part09.rar"},
+			&types.Link{URL: "http://www.uploadable.ch/file/NrcrC9Ps9YKB/thtdos13.ex.1080p-gec.part10.rar"},
+			&types.Link{URL: "http://www.uploadable.ch/file/CVFrewUMWQ2B/thtdos13.ex.1080p-gec.part11.rar"},
+			&types.Link{URL: "http://www.uploadable.ch/file/t8pVzjrKvMcp/thtdos13.ex.1080p-gec.part12.rar"},
+			&types.Link{URL: "http://www.uploadable.ch/file/hX2cHG5c5NNu/thtdos13.ex.1080p-gec.part13.rar"},
+			&types.Link{URL: "http://www.uploadable.ch/file/fGzxbgEPspBD/thtdos13.ex.1080p-gec.part14.rar"},
+		}})
 }
