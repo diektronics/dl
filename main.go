f40a542bd10f2251f54dda8f9a5c732b4b26aa1b
Diego Carretero-Frades <carter@diektronics.com>
2014-10-23 10:21:38 -0700

    hooks working, time for HTML

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"diektronics.com/carter/dl/cfg"
 	"diektronics.com/carter/dl/dl"
 	"diektronics.com/carter/dl/server"
-	"diektronics.com/carter/dl/types"
 )
 
 var cfgFile = flag.String(
@@ -27,26 +26,7 @@ func main() {
 	}
 
 	// TODO(diek): do recovery, all that is RUNNING becomes QUEUED, then all that is QUEUED goes to the queue.
-	d := dl.New(c, 5)
-	server.New(d).Run()
-
-	d.Download(&types.Download{Name: "Test 1",
-		Links: []*types.Link{
-			&types.Link{URL: "http://www.uploadable.ch/file/sNfB6fVsMmy7/test1.part1.rar"},
-			&types.Link{URL: "http://www.uploadable.ch/file/29nM4NpTJ8HH/test1.part2.rar"},
-			&types.Link{URL: "http://www.uploadable.ch/file/AHzPVTgAZExM/test1.part3.rar"},
-		},
-		Posthook: "UNRAR,REMOVE",
-	})
-
-	d.Download(&types.Download{Name: "Test 2",
-		Links: []*types.Link{
-			&types.Link{URL: "http://www.uploadable.ch/file/sNfB6fVsMmy7/test1.part1.rar"},
-			&types.Link{URL: "http://www.uploadable.ch/file/29nM4NpTJ8HH/test1.part2.rar"},
-			&types.Link{URL: "http://www.uploadable.ch/file/AHzPVTgAZExM/test1.part3.rar"},
-		},
-		Posthook: "UNRAR,REMOVE",
-	})
+	server.New(dl.New(c, 5)).Run()
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
