c05327cbded56a80a8314dc8acd2095aba433743
Diego Carretero-Frades <carter@diektronics.com>
2015-05-26 21:23:02 -0700

    make the email a little bit nicer

--- backend/notifier/notifier.go
+++ backend/notifier/notifier.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"net/smtp"
+	"strings"
 
 	"diektronics.com/carter/dl/protos/cfg"
 	dlpb "diektronics.com/carter/dl/protos/dl"
@@ -31,7 +32,14 @@ func New(c *cfg.Configuration) *Client {
 func (n Client) Notify(down *dlpb.Down) {
 	header := fmt.Sprintf("From: %s\nTo: %s\nSubject: %s %v\n\n",
 		n.sender, n.recipient, down.Name, down.Status.String())
-	content := []byte(header + down.String())
+	links := make([]string, 0, len(down.Links))
+	for _, l := range down.Links {
+		links = append(links, fmt.Sprintf("%s: %s", l.Url, l.Status))
+	}
+	body := fmt.Sprintf("Name: %s\nStatus: %s\nErrors:\n\t%s\nPosthook: %s\nDestination: %s\nLinks:\n\t%s\n",
+		down.Name, down.Status, strings.Join(down.Errors, "\n\t"), strings.Join(down.Posthook, ", "),
+		down.Destination, strings.Join(links, "\n\t"))
+	content := []byte(header + body)
 
 	addrPort := net.JoinHostPort(n.addr, fmt.Sprint(n.port))
 
